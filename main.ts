52e7b71575718f1dfd631085e9b8dc93154a3aef
pocky088 <122331821+pocky088@users.noreply.github.com>
2023-01-10 10:36:59 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("TIGGER")
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("WINNIE")
+})
